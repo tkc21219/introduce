2977f149b7e1fe4fe12c60bdb75000129bd984c3
tkcco <asd33tyu@gmail.com>
2018-11-20 12:13:01 +0900

    変更

--- _dev/assets/scripts/snow.js
+++ _dev/assets/scripts/snow.js
@@ -1,89 +1,124 @@
-$(function(){
-  function drop(param){
-    var topWidth = $(window).width(),
-        topHeight = $(window).height();
-    $('.js-openning').css({
-      width: topWidth,
-      height: topHeight
+$(function() {
+
+  function dropObj(param) {
+    this.param = param;
+    this.init();
+  };
+
+  dropObj.prototype.init = function() {
+    var that = this;
+    this.setWindow();
+    this.create();
+    $(window).on('resize',function() {
+      that.play();
     });
-    $(window).on('resize', function(){
-      var topWidth = $(window).width(),
-          topHeight = $(window).height();
-      $('.js-openning').css({
-        width: topWidth,
-        height: topHeight
+  };
+
+  dropObj.prototype.setWindow = function() {
+    var $window = $(window);
+    var winWidth = $window.width();
+    var winHeight = $window.height();
+    var $openning = $('.js-openning');
+    $openning.css({
+      width: winWidth,
+      height: winHeight
+    });
+    $window.on('resize', function() {
+      var winWidth = $window.width();
+      var winHeight = $window.height();
+      $openning.css({
+        width: winWidth,
+        height: winHeight
       });
     });
-    //画像を生成
-    var create = function() {
-      var img = document.createElement('img');
-      img.src = param.imgpath;
-      $(img).on('load',function(){
-        for (var i = 0; i < param.objectMax; i += 1) {
-          $(param.wrap).append('<img src="' + param.imgpath + '" class="' + param.targetClass + '" />');
-        }
-        play();
-      });
-    };
-    //落ちる動きの処理
-    var move = function(obj) {
-      var xPosition = Math.floor(Math.random() * $(window).width()),
-          yPosition = {
-            start:Math.random() * 1000 * -1,
-            end:$(window).height()
-          },
-          size = Math.floor(Math.random() * (param.size.max - param.size.min) + param.size.min),
-          time = Math.floor(Math.abs(yPosition.start - yPosition.end) * (param.speed / 480)) * (param.size.max / size);
-      $(obj).stop()
-        .css({width: size + 'px', top: yPosition.start + 'px', left: xPosition + 'px' ,position:'fixed'})
-        .animate({ top: yPosition.end + 'px'}, { duration:time, queue:false, easing:'linear',
-                                                 complete:function(){
-                                                   move(this);
-                                                 }});
-      swing(obj);
-    };
-    //揺れる動きの処理
-    var swing = function(obj) {
-      var swingNum = Math.floor(Math.random() * (param.swing.max - param.swing.min) + param.swing.min) * -1;
-      $(obj).animate({marginLeft: swingNum + 'px'}, {duration:param.swing.speed, queue:false,
-                                                     complete:function(){
-                                                       $(obj).animate({marginLeft: 0}, {duration:param.swing.speed, queue:false,
-                                                                                        complete:function(){
-                                                                                          swing(this);
-                                                                                        }});
-                                                     }});
-    };
-    //再生
-    var play = function(){
-      $('.' + param.targetClass).each(function(){
-        move(this);
-      });
-    };
-    //init
-    var init = function(){
-      create();
-      $(window).on('resize',function(){
-        play();
-      });
-    };
-    init();
-  }
-  $(document).ready(function() {
-    drop({
-      wrap:'.js-openning',
-      //配置する場所
-      targetClass: 'snow',
-      //オブジェクトのクラス
-      size: {max: 40, min: 10 },
-      //オブジェクトの最大サイズ
-      objectMax: 70,
-      //オブジェクトの数
-      speed: 3000,
-      //オブジェクトの速度
-      swing: {max: 25, min: .5, speed: 850},
-      //オブジェクトの揺れ幅
-      imgpath: '/introduce/myprofile/assets/images/diamond.png'
-      //オブジェクトの画像パス
+  };
+
+  //画像を生成
+  dropObj.prototype.create = function() {
+    var that = this;
+    var img = document.createElement('img');
+    img.src = this.param.imgpath;
+    $(img).on('load',function() {
+      for (var i = 0; i < that.param.objectMax; i += 1) {
+        $(that.param.wrap).append('<img src="' + that.param.imgpath + '" class="' + that.param.targetClass + '" />');
+      }
+      that.play();
     });
+  };
+
+  //再生
+  dropObj.prototype.play = function() {
+    var that = this;
+    $('.' + this.param.targetClass).each(function(i, item) {
+      that.move(item);
+    });
+  };
+
+  //落ちる動きの処理
+  dropObj.prototype.move = function(obj) {
+    var that = this;
+    var xPosition = Math.floor(Math.random() * $(window).width());
+    var yPosition = {
+      start: Math.random() * 1000 * -1,
+      end: $(window).height()
+    };
+    var size = Math.floor(Math.random() * (this.param.size.max - this.param.size.min) + this.param.size.min);
+    var time = Math.floor(Math.abs(yPosition.start - yPosition.end) * (this.param.speed / 480)) * (this.param.size.max / size);
+    $(obj).stop().css({
+      width: size + 'px',
+      top: yPosition.start + 'px',
+      left: xPosition + 'px',
+      position: 'fixed'
+    }).animate({
+      top: yPosition.end + 'px'
+    }, {
+      duration: time,
+      queue: false,
+      easing: 'linear',
+      complete: function() {
+        that.move(this);
+      }
+    });
+    this.swing(obj);
+  };
+
+  //揺れる動きの処理
+  dropObj.prototype.swing = function(obj) {
+    var that = this;
+    var swingNum = Math.floor(Math.random() * (this.param.swing.max - this.param.swing.min) + this.param.swing.min) * -1;
+    $(obj).animate({
+      marginLeft: swingNum + 'px'
+    }, {
+      duration: this.param.swing.speed,
+      queue: false,
+      complete: function() {
+        $(obj).animate({
+          marginLeft: 0
+        }, {
+          duration: that.param.swing.speed,
+          queue: false,
+          complete: function() {
+            that.swing(this);
+          }
+        });
+      }
+    });
+  };
+
+  new dropObj({
+    wrap:'.js-openning',
+    //配置する場所
+    targetClass: 'snow',
+    //オブジェクトのクラス
+    size: {max: 40, min: 10 },
+    //オブジェクトの最大サイズ
+    objectMax: 70,
+    //オブジェクトの数
+    speed: 3000,
+    //オブジェクトの速度
+    swing: {max: 25, min: .5, speed: 850},
+    //オブジェクトの揺れ幅
+    imgpath: '/introduce/myprofile/assets/images/diamond.png'
+    //オブジェクトの画像パス
   });
 });
